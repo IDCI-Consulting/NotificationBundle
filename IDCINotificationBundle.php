f702ed5276b9680c9b2f3cfbffb1237a8a2f3d43
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2013-07-12 11:40:43 +0200

    Add notification bundle class

---
+++ IDCINotificationBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * 
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @license: GPL
+ *
+ */
+
+namespace IDCI\Bundle\NotificationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class IDCINotificationBundle extends Bundle
+{
+}
