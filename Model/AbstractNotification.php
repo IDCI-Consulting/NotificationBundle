1415be18126b183bc57d4962d0b8deca2e9b5cba
Sekou KOITA <sictoz@gmail.com>
2013-07-19 11:33:57 +0200

    Update AbstractNotification

--- Model/AbstractNotification.php
+++ Model/AbstractNotification.php
@@ -15,6 +15,12 @@ use IDCI\Bundle\NotificationBundle\Entity\Notification;
 abstract class AbstractNotification implements NotificationInterface
 {
     protected $from;
+    protected $type;
+    protected $source;
+    protected $createdAt;
+    protected $updatedAt;
+    protected $status;
+    protected $log;
 
     public function getFrom()
     {
@@ -28,6 +34,65 @@ abstract class AbstractNotification implements NotificationInterface
         return $this;
     }
 
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    public function getSource()
+    {
+        return $this->source;
+    }
+
+    public function setSource($source)
+    {
+        $this->source = $source;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    public function getsStatus()
+    {
+        return $this->status;
+    }
+
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    public function getLog()
+    {
+        return $this->log;
+    }
+
+    public function setLog($log)
+    {
+        $this->log = $log;
+    }
     /**
      * @see NotificationInterface
      */
@@ -35,6 +100,12 @@ abstract class AbstractNotification implements NotificationInterface
     {
         $this
             ->setFrom($notificationEntity->getFrom())
+            ->setType($notificationEntity->getType())
+            ->setSource($notificationEntity->getSource())
+            ->setCreatedAt($notificationEntity->getCreatedAt())
+            ->setUpdatedAt($notificationEntity->getUpdatedAt())
+            ->setStatus($notificationEntity->getStatus())
+            ->setLog($notificationEntity->getLog())
         ;
     }
 
