bf57d2afd8378040837bfad532c706f9ff1c9e4a
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2013-07-30 19:54:36 +0200

    Add CRUD lite controller (list + show) generated with doctrine:generate:crud

---
+++ Controller/AdminController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace IDCI\Bundle\NotificationBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use IDCI\Bundle\NotificationBundle\Entity\Notification;
+
+/**
+ * Notification controller.
+ *
+ * @Route("/")
+ */
+class AdminController extends Controller
+{
+
+    /**
+     * Lists all Notification entities.
+     *
+     * @Route("/", name="")
+     * @Method("GET")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('IDCINotificationBundle:Notification')->findAll();
+
+        return array(
+            'entities' => $entities,
+        );
+    }
+
+    /**
+     * Finds and displays a Notification entity.
+     *
+     * @Route("/{id}", name="_show")
+     * @Method("GET")
+     * @Template()
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('IDCINotificationBundle:Notification')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Notification entity.');
+        }
+
+        return array(
+            'entity'      => $entity,
+        );
+    }
+}
