74b950622fd3cf90799e59b4964bd6cc7c8934fb
PichetPuth <pichet.puth@gmail.com>
2014-02-24 15:54:16 +0100

    Add form fields checking

--- Notifier/AbstractNotifier.php
+++ Notifier/AbstractNotifier.php
@@ -11,9 +11,44 @@
 namespace IDCI\Bundle\NotificationBundle\Notifier;
 
 use IDCI\Bundle\NotificationBundle\Util\Inflector;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 abstract class AbstractNotifier implements NotifierInterface
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function cleanData($data)
+    {
+        foreach ($data as $field => $options) {
+            $fieldOptions = $this->getFieldOptions($field);
+            $data[$field] = $this->getResolver($fieldOptions)->resolve($options);
+        }
+
+        return $data;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getResolver(array $options)
+    {
+        $resolver = new OptionsResolver();
+        $this->setDefaultOptions($resolver, $options);
+
+        return $resolver;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFieldOptions($field)
+    {
+        $method = sprintf('get%sFields', ucfirst(strtolower($field)));
+
+        return $this->$method();
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -28,5 +63,34 @@ abstract class AbstractNotifier implements NotifierInterface
     public function getContentFields()
     {
         return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFromFields()
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolver $resolver, array $fieldOptions)
+    {
+        foreach ($fieldOptions as $name => $options) {
+            $isRequired = isset($options[1]) && isset($options[1]['required']) ? $options[1]['required'] : false;
+            if ($isRequired) {
+                $resolver->setRequired(array($name));
+            } else {
+                $resolver->setOptional(array($name));
+            }
+            $hasChoices = isset($options[1]) && isset($options[1]['choices']) && count($options[1]['choices']) > 0 ? true : false;
+            if ($hasChoices) {
+                $resolver->setAllowedValues(array(
+                    $name => array_keys($options[1]['choices'])
+                ));
+            }
+        }
     }
 }
--- Notifier/NotifierInterface.php
+++ Notifier/NotifierInterface.php
@@ -4,6 +4,7 @@
  * 
  * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
  * @author:  Sekou KOÏTA <sekou.koita@supinfo.com>
+ * @author:  Pichet PUTH <pichet.puth@utt.fr>
  * @license: GPL
  *
  */
@@ -11,6 +12,7 @@
 namespace IDCI\Bundle\NotificationBundle\Notifier;
 
 use IDCI\Bundle\NotificationBundle\Entity\Notification;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 interface NotifierInterface
 {
@@ -18,6 +20,7 @@ interface NotifierInterface
      * Send notification
      *
      * @param Notification $notification
+     *
      * @return boolean
      */
     public function sendNotification(Notification $notification);
@@ -35,4 +38,47 @@ interface NotifierInterface
      * @return array|false
      */
     public function getContentFields();
+
+    /**
+     * Get From Fields
+     *
+     * @return array|false
+     */
+    public function getFromFields();
+
+    /**
+     * Clean data
+     *
+     * @param array $data The data to be checked
+     *
+     * @return array The cleaned data
+     */
+    public function cleanData($data);
+    
+    /**
+     * Get resolver
+     *
+     * @param array $options 
+     *
+     * @return $resolver
+     */
+    public function getResolver(array $options);
+    
+    /**
+     * Get field option
+     *
+     * @param string $field The name of field("to", "from", "content")
+     *
+     * @return array 
+     */
+    public function getFieldOptions($field);
+    
+    /**
+     * Configure OptionResolver with default options
+     *
+     * @param OptionsResolver $resolver     
+     * @param array           $fieldOptions
+     */
+    public function setDefaultOptions(OptionsResolver $resolver, array $fieldOptions);
+    
 }
