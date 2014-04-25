95b460b22975f66adfd3284c66397b275ee6297e
PichetPuth <pichet.puth@gmail.com>
2014-04-25 18:13:00 +0200

    Work on IOSPushNotification

--- Notifier/IOSPushNotifier.php
+++ Notifier/IOSPushNotifier.php
@@ -3,7 +3,6 @@
 /**
  *
  * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
- * @author:  Sekou KOÏTA <sekou.koita@supinfo.com>
  * @author:  Pichet PUTH <pichet.puth@utt.fr>
  * @license: GPL
  *
@@ -12,9 +11,7 @@
 namespace IDCI\Bundle\NotificationBundle\Notifier;
 
 use IDCI\Bundle\NotificationBundle\Entity\Notification;
-use IDCI\Bundle\NotificationBundle\Exception\NotificationFieldParseErrorException;
-use IDCI\Bundle\NotificationBundle\Exception\InitSocketConnexionFailedException;
-use IDCI\Bundle\NotificationBundle\Exception\SendingErrorViaSocketConnexionException;
+use IDCI\Bundle\NotificationBundle\Exception\IOSPushNotifierException;
 
 class IOSPushNotifier extends AbstractNotifier
 {
@@ -23,31 +20,15 @@ class IOSPushNotifier extends AbstractNotifier
      */
     public function sendNotification(Notification $notification)
     {
-        //$notification->getTo()
-        //$notification->getTo('deviceToken')
-        $deviceToken = json_decode($notification->getTo(), true);
-        if (null === $deviceToken) {
-            throw new NotificationFieldParseErrorException($notification->getTo());
-        }
+        $socket = self::createSocketConnexion($notification->getFrom('passphrase'));
 
-        $passphrase = json_decode($notification->getFrom(), true);
-        if (null === $passphrase) {
-            throw new NotificationFieldParseErrorException($notification->getFrom());
-        }
-
-        $message = json_decode($notification->getContent(), true);
-        if (null === $message) {
-            throw new NotificationFieldParseErrorException($notification->getContent());
-        }
-
-        // Encode the payload as JSON
-        $payload = json_encode($this->createPayloadBody($message['message']));
-        // Build the binary notification
-        $msg = $this->buildBinaryNotification($deviceToken['deviceToken'], $payload);
-
-        $fp = sefl::initSocketConnexion($passphrase['passphrase']);
-        sefl::sendViaSocketConnexion($fp, $msg);
-
+        return self::sendBinaryMessage(
+            $socket,
+            $this->buildBinaryMessage(
+                $notification->getTo('deviceToken'),
+                $notification->getContent('message')
+            )
+        );
     }
 
     /**
@@ -56,79 +37,61 @@ class IOSPushNotifier extends AbstractNotifier
      * @param string $passphrase
      * @thrown SocketConnexionFailedException
      */
-    public static function initSocketConnexion($passphrase)
+    public static function createSocketConnexion($passphrase)
     {
-        $context = stream_context_create(); //Crée un contexte de flux
-        //Configure une option pour un flux/gestionnaire/contexte
-        stream_context_set_option($context, 'ssl', 'local_cert', 'ck.pem');
-        stream_context_set_option($context, 'ssl', 'passphrase', $passphrase);
-        // Open a connection to the APNS server
-        // Ouvre une connexion socket Internet ou Unix
-        // return false if connection error
-        $fp = stream_socket_client(
+        $ctx = stream_context_create();
+        stream_context_set_option($ctx, 'ssl', 'local_cert', '/home/quentin/workspace/NotificationManager/bin/pp.pem');
+        stream_context_set_option($ctx, 'ssl', 'passphrase', $passphrase);
+
+        $socket = stream_socket_client(
             'ssl://gateway.sandbox.push.apple.com:2195',
             $err,
             $errstr,
             60,
             STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT,
-            $context
+            $ctx
         );
 
-        if (!$fp) {
-            throw new InitSocketConnexionFailedException($err, $errstr);
+        if (!$socket) {
+            throw new IOSPushNotifierException(sprintf(
+                'Connection failed: %s, %s',
+                $err,
+                $errstr
+            ));
         }
 
-        return $fp;
+        return $socket;
     }
 
     /**
      * Send the payload using socket connexion
      *
-     * @param TODO   $fp
-     * @param string $msg
-     * @throw SendingErrorViaSocketConnexionException
+     * @param  TODO   $socket
+     * @param  string $binaryMessage
+     * @return boolean
      */
-    public static function sendViaSocketConnexion($fp, $msg)
+    public static function sendBinaryMessage($socket, $binaryMessage)
     {
-        // Send it to the server
-        // Écrit un fichier en mode binaire
-        $result = fwrite($fp, $msg, strlen($msg));
+        $result = fwrite($socket, $binaryMessage, strlen($binaryMessage));
+        fclose($socket);
 
-        if (!$result) {
-            throw new SendingErrorViaSocketConnexionException()
-        }
-
-        // Close the connection to the server
-        // Ferme un fichier
-        fclose($fp);
-    }
-
-
-    /**
-     * Create the payload body
-     *
-     * @param string $message
-     * @return array $body
-     */
-    protected function createPayloadBody($message)
-    {
-        $body['aps'] = array(
-            'alert' => $message,
-            'sound' => 'default'
-        );
-
-        return $body;
+        return false === $result ? false : true;
     }
 
     /**
      * Build the binary notification
      *
      * @param string $deviceToken
-     * @param string $payload
+     * @param string $message
      * @return string
      */
-    protected function buildBinaryNotification($deviceToken, $payload)
+    protected function buildBinaryMessage($deviceToken, $message)
     {
+        $payload = json_encode(array(
+            'alert' => $message,
+            'sound' => 'default'
+        ));
+
         return chr(0) . pack('n', 32) . pack('H*', $deviceToken) . pack('n', strlen($payload)) . $payload;
     }
 
@@ -148,7 +111,7 @@ class IOSPushNotifier extends AbstractNotifier
     public function getFromFields()
     {
         return array(
-            'passphrase' => array('text', array('required' => true))
+            'passphrase' => array('password', array('required' => true))
         );
     }
 
