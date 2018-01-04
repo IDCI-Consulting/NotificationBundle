f2e6bebdbe1c798fe90fed71c55bfe845a9b05ac
Mencé <remy.mence@tessi.fr>
2018-01-04 17:59:42 +0100

    Add tags on notifier configuration

--- Entity/Repository/NotifierConfigurationRepository.php
+++ Entity/Repository/NotifierConfigurationRepository.php
@@ -12,4 +12,58 @@ namespace IDCI\Bundle\NotificationBundle\Entity\Repository;
  */
 class NotifierConfigurationRepository extends AbstractEntityRepository
 {
+    /**
+     * Extract tags data
+     *
+     * @param array $tags
+     *
+     * @return array
+     */
+    public function extractTagsData(array $tags)
+    {
+        if (empty($tags)) {
+            return array();
+        }
+        $extractedTags = array();
+        foreach ($tags as $tag) {
+            $tag = JsonHandler::decode($tag, true);
+            $extractedTags[] = $tag;
+        }
+        return $extractedTags;
+    }
+
+    /**
+     * Find templates.
+     *
+     * @param array   $tags
+     * @param string   $alias
+     *
+     * @return QueryBuilder
+     */
+    public function findNotifierConfigurationQueryBuilder(array $tags, $alias = 'notifierConfiguration')
+    {
+        $qb = $this->createQueryBuilder($alias);
+        if (!empty($tags)) {
+            $this->findbyTagsQueryBuilder($tags, $qb, $alias);
+        }
+
+        return $qb;
+    }
+
+    /**
+     * @param array        $tags
+     * @param QueryBuilder $qb
+     * @param string       $alias
+     *
+     * @return QueryBuilder
+     */
+    public function findByTagsQueryBuilder(array $tags, QueryBuilder $qb = null, $alias = 'notifierConfiguration')
+    {
+        if (null === $qb) {
+            $qb = $this->createQueryBuilder($alias);
+        }
+        $tags = $this->extractTagsData($tags);
+        MetadataRepository::findByMetadataQueryBuilder($qb, sprintf('%s.tags', $alias), $tags);
+        return $qb;
+    }
 }
